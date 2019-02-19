8f4e2a0efaf872db7c9f771d012e02ee357ec482
Thomas Capelle <thomascapelle@gmail.com>
2019-02-19 13:53:15 +0000

    Focal loss in utils.py

---
+++ utils.py
@@ -0,0 +1,22 @@
+import torch
+import torch.functional as F
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+
+    def forward(self, inputs, targets, reduction=True):
+        if self.logits:
+            CE_loss = F.cross_entropy_with_logits(inputs, targets, reduction='none')
+        else:
+            CE_loss = F.cross_entropy(inputs, targets, reduction='none')
+        pt = torch.exp(-CE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * CE_loss
+
+        if reduction:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
